65ed7b0eff8068c3c9efacdbdc929bc9282811f5
sokkim roth <34774632+kimsok@users.noreply.github.com>
2024-06-18 21:53:19 +0700

    update route prefix to api

--- routes/api.go
+++ routes/api.go
@@ -10,15 +10,16 @@ import (
 var RegisterRoutes = func(r *mux.Router) {
 	room := &controller.Rooms{}
 	booking := &controller.RoomBooking{}
-	r.HandleFunc("/rooms", room.Index).Methods(http.MethodGet)
-	r.HandleFunc("/rooms/{id:[0-9]+}", room.Show).Methods(http.MethodGet)
-	r.HandleFunc("/rooms", room.Store).Methods(http.MethodPost)
-	r.HandleFunc("/rooms/{id:[0-9]+}", room.Update).Methods(http.MethodPut)
-	r.HandleFunc("/rooms/{id:[0-9]+}", room.Delete).Methods(http.MethodDelete)
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/rooms", room.Index).Methods(http.MethodGet)
+	api.HandleFunc("/rooms/{id:[0-9]+}", room.Show).Methods(http.MethodGet)
+	api.HandleFunc("/rooms", room.Store).Methods(http.MethodPost)
+	api.HandleFunc("/rooms/{id:[0-9]+}", room.Update).Methods(http.MethodPut)
+	api.HandleFunc("/rooms/{id:[0-9]+}", room.Delete).Methods(http.MethodDelete)
 
-	r.HandleFunc("/room-bookings", booking.Index).Methods(http.MethodGet)
-	r.HandleFunc("/room-bookings/{id:[0-9]+}", booking.Show).Methods(http.MethodGet)
-	r.HandleFunc("/room-bookings", booking.Store).Methods(http.MethodPost)
-	r.HandleFunc("/room-bookings/{id:[0-9]+}", booking.Update).Methods(http.MethodPut)
-	r.HandleFunc("/room-bookings/{id:[0-9]+}", booking.Delete).Methods(http.MethodDelete)
+	api.HandleFunc("/room-bookings", booking.Index).Methods(http.MethodGet)
+	api.HandleFunc("/room-bookings/{id:[0-9]+}", booking.Show).Methods(http.MethodGet)
+	api.HandleFunc("/room-bookings", booking.Store).Methods(http.MethodPost)
+	api.HandleFunc("/room-bookings/{id:[0-9]+}", booking.Update).Methods(http.MethodPut)
+	api.HandleFunc("/room-bookings/{id:[0-9]+}", booking.Delete).Methods(http.MethodDelete)
 }
